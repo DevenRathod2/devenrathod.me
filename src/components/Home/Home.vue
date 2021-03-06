c57524dea99eef51bb04f12c35c9985fdefad9d2
DevenRathod2 <devenrathod2001@gmail.com>
2021-03-06 15:43:04 +0530

    make button clickable

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -14,22 +14,31 @@
           </p>
         </div>
         <div class="social-container">
+          <a href="https://github.com/devenrathod2" target="https://github.com/devenrathod2">
           <div class="github">
             <img src="" alt="" />
             <h3 class="social-title">I'm On Github</h3>
           </div>
+          </a>
+          <a href="https://twitter.com/IamDevenR"  target="https://twitter.com/IamDevenR">
           <div class="twitter">
             <img src="" alt="" />
             <h3 class="social-title">Also On Twitter</h3>
           </div>
+          </a>
+          <a href="https://www.linkedin.com/in/devenrathod"  target="https://www.linkedin.com/in/devenrathod">
           <div class="linkedin">
             <img src="" alt="" />
             <h3 class="social-title">Lets Connect Here</h3>
           </div>
-          <div class="instagram">
+          </a>
+          <a href="https://instagram.com/the_exploitz"  target="https://instagram.com/the_exploitz">
+            <div class="instagram">
             <img src="" alt="" />
             <h3 class="social-title">Follow me Instagram</h3>
           </div>
+          </a>
+          
           <div class="footer-text">
             <p>Design with <span class="highlight-text-red">&#10084;</span> By <span class="highlight-text">Deven Rathod</span></p>
           </div>
@@ -41,6 +50,7 @@
 </template>
 
 <script>
+
 export default {
   name: "DevenRathod",
   props: {

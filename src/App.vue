92f871118e8386897448ef47944f0f6b32210cf8
Deven Rathod <devenrathod2001@gmail.com>
2021-04-28 00:10:23 +0530

    ✨ Added Particles Background

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
     <Home/>
+    <particles-bg color="#45d1a2" num=30 type="cobweb" :canvas="{backgroundColor:'#2A2E35'}" :bg="true"/>
   </div>
 </template>
 <script>
 import Home from "./components/Home/Home.vue";
+import { ParticlesBg } from "particles-bg-vue";
 
 export default {
   name: "App",
   components: {
     Home,
+    ParticlesBg
   },
 };
 </script>
 
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Open+Sans&family=Poppins:wght@200&display=swap');
 #app {
-  font-family: "Poppins", sans-serif;
+  font-family: 'Josefin Sans', sans-serif;
+  font-family: 'Open Sans', sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

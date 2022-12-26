711437ec17d07aae88ba0be64855df491774accb
= <devenrathod2001@gmail.com>
2022-12-26 19:26:37 +0530

    Added Button

--- components/Home.vue
+++ components/Home.vue
@@ -23,24 +23,34 @@
                     <h2 class="tracking-in-contract-bck">Hey, I’m</h2>
                 </div>
                 <div class="title-container">
-                    <h1 class="tracking-in-contract-bck">Deven S. Rathod</h1>
+                    <h1 class="tracking-in-contract-bck">Deven Rathod</h1>
                 </div>
                 <div class="bio-container tracking-in-contract">
-                    <p>I'm a software engineer based in Toronto, Canada and also a  communication and journalism student.
-                        I enjoy creating things that live on the internet,  whether that be websites, applications, or
-                        anything in between. I have been freelancing  for a year now while studying at the university and
+                    <p>I'm a software engineer based in India. I enjoy creating things that live on the internet,  whether that be websites, applications, or
+                        anything in between. I have been freelancing for a year now while studying at the university and
                         I've manage to  gain a decent amount of experience and valuable knowledge from all different
                         kinds of fields throughout my projects/work.</p>
                 </div>
                 <div class="say-hi-container">
-                    <h3 class="tracking-in-contract-bck">Say hi</h3>
+                    <h3 class="tracking-in-contract-bck">Say hi</h3> <img src="../assets/arrow.svg"></img>
                 </div>
                 <div class="footer-btn">
                     <div class="btn-container">
-                        <button class="email-btn">Send an email</button>
-                        <button class="linkedin-btn">LinkedIn</button>
-                        <button class="upwork-btn">Upwork</button>
-                        <button class="github-btn">Github</button>
+                        <button class="email-btn">
+                            <img src="../assets/mail.svg"></img>
+                            <h3>Send an email</h3></button>
+                        <button class="linkedin-btn">
+                            <img src="../assets/linkedin.svg"></img>
+                            <h3>LinkedIn</h3>
+                            </button>
+                        <button class="upwork-btn">
+                            <img src="../assets/upwork.svg"></img>
+                            <h3>Upwork</h3>
+                            </button>
+                        <button class="github-btn">
+                            <img src="../assets/github.svg"></img>
+                            <h3>Github</h3>
+                            </button>
                     </div>
                 </div>
             </div>

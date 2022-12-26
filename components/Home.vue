a4c038cc9ce1dd9530f58b39e682b55b79b90961
= <devenrathod2001@gmail.com>
2022-12-26 06:51:23 +0530

    Upgrade from Vue 2 to Vue 3 with NuxtJS

---
+++ components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+        <header>
+            <div class="navbar">
+                <div class="logo">
+                    <h2>DR</h2>
+                </div>
+                <div class="menu">
+                    <nav>
+                        <ul>
+                            <li>Project</li>
+                            <li>Resume</li>
+                            <li>Contact</li>
+                        </ul>
+
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <section>
+            <div class="container">
+                <div class="hi-container">
+                    <h2 class="tracking-in-contract-bck">Hey, I’m</h2>
+                </div>
+                <div class="title-container">
+                    <h1 class="tracking-in-contract-bck">Deven S. Rathod</h1>
+                </div>
+                <div class="bio-container tracking-in-contract">
+                    <p>I'm a software engineer based in Toronto, Canada and also a  communication and journalism student.
+                        I enjoy creating things that live on the internet,  whether that be websites, applications, or
+                        anything in between. I have been freelancing  for a year now while studying at the university and
+                        I've manage to  gain a decent amount of experience and valuable knowledge from all different
+                        kinds of fields throughout my projects/work.</p>
+                </div>
+                <div class="say-hi-container">
+                    <h3 class="tracking-in-contract-bck">Say hi</h3>
+                </div>
+                <div class="footer-btn">
+                    <div class="btn-container">
+                        <button class="email-btn">Send an email</button>
+                        <button class="linkedin-btn">LinkedIn</button>
+                        <button class="upwork-btn">Upwork</button>
+                        <button class="github-btn">Github</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</template>
+
+<style>
+@import url('../assets/style.css');
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <Tutorial/> -->
+  <Home/>
+</template>
+
+<script>
+import Home from '../components/Home.vue';
+
+export default {
+    name: "IndexPage",
+    components: { Home }
+}
+</script>
